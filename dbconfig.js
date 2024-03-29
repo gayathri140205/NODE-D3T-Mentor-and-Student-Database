1c03e3a2fb51073a07c427899ba054996e1a288b
gayathri140205 <gayathri140205@gmail>
2024-03-29 23:32:43 +0530

    updated

--- dbconfig.js
+++ dbconfig.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 
-
 function connectToDatabase(){
-    const URI = 
-    process.env.NODE_ENV === "development"
-     ?process.env.MONGODB_URI
-     :"mongodb://localhost:27017/taskee3";
-    console.log(process.env.NODE_ENV);
-   mongoose
-   .connect (URI)
-   .then((response)=>{
-      if (response) console.log("database connection successful");
-   })
-   .catch((err)=> console.log("database connection failed",err));
+   const URI = 
+   process.env.NODE_ENV === "development"
+    ?process.env.MONGODB_URI
+    :"mongodb://localhost:27017/taskee3";
+   console.log(process.env.NODE_ENV);
+  mongoose
+  .connect (URI)
+  .then((response)=>{
+     if (response) console.log("database connection successful");
+  })
+  .catch((err)=> console.log("database connection failed",err));
 }
 module.exports ={
-  connectToDatabase,
+ connectToDatabase,
 };
